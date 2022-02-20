fae4d4a4dec21487c19dc9d764b2bd2109b052bb
Rajesh <wraejesh@gmail.com>
2022-02-20 14:12:26 +0530

    modified defaults for client.js

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -5,7 +5,8 @@ exports.WhatsWebURL = 'https://web.whatsapp.com/';
 exports.DefaultOptions = {
     puppeteer: {
         headless: true,
-        defaultViewport: null
+        defaultViewport: null,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
     },
     session: false,
     qrTimeoutMs: 45000,
@@ -16,7 +17,7 @@ exports.DefaultOptions = {
     takeoverTimeoutMs: 0,
     userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36',
     ffmpegPath: 'ffmpeg',
-    bypassCSP: false
+    bypassCSP: false,
 };
 
 /**
